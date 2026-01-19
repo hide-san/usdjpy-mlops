75fd0c6a05c86dd65c81d9fd9689df677af2bdb3
hideo <hideo@sato.name>
2026-01-20 04:52:15 +0900

    Adding files to process an analyze fetched data

--- fetch.py
+++ fetch.py
@@ -1,18 +1,30 @@
 import requests
 import pandas as pd
+import json
+from datetime import datetime, timedelta
 import os
 
 API_KEY = os.getenv("EXCHANGE_API_KEY")
-API_URL = f"https://api.exchangerate.host/live?access_key={API_KEY}&symbols=JPY"
+MAX_DATE_WINDOW=365
+
+def get_dates():
+    df = pd.read_csv("data/usdjpy.csv")
+    start = df['date'].max()
+    date_format = "%Y-%m-%d"
+    date_obj = datetime.strptime(start, date_format)
+    end = date_obj + timedelta(days=MAX_DATE_WINDOW)
+    return start, end.strftime(format=date_format)
+
+
+start_date, end_date = get_dates()
+API_URL = f"https://api.exchangerate.host/timeframe?access_key={API_KEY}&source=USD&currencies=JPY&start_date={start_date}&end_date={end_date}"
 
 
 def fetch_usdjpy():
     r = requests.get(API_URL)
     data = r.json()
-    rate = data["quotes"]["USDJPY"]
-    date = data["timestamp"]
-
-    df = pd.DataFrame([{"date": date, "usdjpy": rate}])
+    rows = [{"date": d, "usdjpy": v.get("USDJPY")} for d, v in data["quotes"].items()]
+    df = pd.DataFrame(rows)
 
     os.makedirs("data", exist_ok=True)
     path = "data/usdjpy.csv"
