5d71d21718ccdf86c2338b18e060e23601ccae1c
Hideo Sato <hello@hideo.click>
2026-02-06 02:55:10 +0900

    Delete scripts/commit_data.sh

--- scripts/commit_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-git config --local user.email "actions@github.com"
-git config --local user.name "GitHub Actions"
-git add data/usdjpy.csv
-git commit -m "Update data" || echo "No changes"
-git push
